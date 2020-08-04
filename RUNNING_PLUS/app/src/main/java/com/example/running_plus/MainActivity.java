c211aadbf1b70009c98499349fada0c1454352b3
rodvkf72 <48707324+rodvkf72@users.noreply.github.com>
2020-08-04 16:36:21 +0900

    플 스크린으로 변경
    플 스크린으로 변경
    http -> https로 변경

--- RUNNING_PLUS/app/src/main/java/com/example/running_plus/MainActivity.java
+++ RUNNING_PLUS/app/src/main/java/com/example/running_plus/MainActivity.java
@@ -3,17 +3,20 @@ package com.example.running_plus;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
 
     private WebView mWebView;
-    private final String urladdr = "http://www.runningplus.net/Main/Index.nm";
+    private final String urladdr = "https://www.runningplus.net/Main/Index.nm";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
         mWebView = findViewById(R.id.testWebview);
